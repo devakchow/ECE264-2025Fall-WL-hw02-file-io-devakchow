8779648abe899f81be167a38e5187aefa075578b
Devak <devak.charapalle@gmail.com>
2025-09-07 07:44:57 -0400

    Initial Commit

---
+++ fileint.c
@@ -0,0 +1,24 @@
+/* PLEASE EDIT THIS FILE */
+
+#include "fileint.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+
+bool addFile(char *filename, int *sum) {
+  /* BEGIN STUDENT ANSWER */
+
+  // complete as described in the README
+
+  /* END STUDENT ANSWER */
+  return true;
+}
+
+bool writeSum(char *filename, int sum) {
+  /* BEGIN STUDENT ANSWER */
+
+  // complete as described in the README
+
+  /* END STUDENT ANSWER */
+  return true;
+}
